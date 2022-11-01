0b4bc3b40dd01f59f964f4b0f5251e06dd227669
Franklin Okolie <franklinokolie86@gmail.com>
2022-11-01 07:07:43 +0100

    feat: finished it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,44 +3,63 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='Wrapper'>
-        <div id='slack'>
-            VoN
-        </div>
-        <div className='share'>
-            <img src="/img/share-dsk.svg" alt="" className='share-desktop' />
-            <img src="/img/share-mob.svg" alt="" className='share-mobile' />
-        </div>
-        <div className='AviWrapper'>
-            <img src='/img/von.jpg' alt="" id='profile__img' className='avi'/>
-            <img src="/img/hover-img.svg" alt="upload" className='upload' />
-        </div>
+    <div className="Wrapper">
+      <div id="slack">Franklin Okolie</div>
+      <div className="share">
+        <img src="/img/share-dsk.svg" alt="" className="share-desktop" />
+        <img src="/img/share-mob.svg" alt="" className="share-mobile" />
+      </div>
+      <div className="AviWrapper">
+        <img src="/img/profile.jpg" alt="" id="profile__img" className="avi" />
+        <img src="/img/hover-img.svg" alt="upload" className="upload" />
+      </div>
 
-        <h3 className='my-name'>VoN</h3>
+      <h3 className="my-name">Franklin Okolie</h3>
 
-        <div className='ButtonsWrapper'>
-            <a href="https://twitter.com/imrvon" id='twitter'>Twitter Link</a>
-            <a href="https://training.zuri.team/" id='btn__zuri'>Zuri Team</a>
-            <a href="http://books.zuri.team/" id='books'>Zuri Books</a>
-            <a href="https://books.zuri.team/python-for-beginners?ref_id=VoN" id='book__python'>Python Books</a>
-            <a href="https://background.zuri.team/" id='pitch'>Background Check for Coders</a>
-            <a href="https://books.zuri.team/design-rules" id='book__design'>Design Books</a>
+      <div className="ButtonsWrapper">
+        <a href="https://twitter.com/DeveloperAspire" id="twitter">
+          Twitter Link
+        </a>
+        <a href="https://training.zuri.team/" id="btn__zuri">
+          Zuri Team
+        </a>
+        <a href="http://books.zuri.team/" id="books">
+          Zuri Books
+        </a>
+        <a
+          href="https://books.zuri.team/python-for-beginners?ref_id=Franklin Okolie"
+          id="book__python"
+        >
+          Python Books
+        </a>
+        <a href="https://background.zuri.team/" id="pitch">
+          Background Check for Coders
+        </a>
+        <a href="https://books.zuri.team/design-rules" id="book__design">
+          Design Books
+        </a>
+      </div>
+
+      <div className="slack-git">
+        <img src="/img/slack.svg" alt="" />
+        <img src="/img/github.svg" alt="" />
+      </div>
+
+      <hr />
+
+      <footer>
+        <div>
+          <img src="/img/zuri.svg" alt="Zuri" />
         </div>
-
-        <div className='slack-git'>
-            <img src="/img/slack.svg" alt="" />
-            <img src="/img/github.svg" alt="" />
+        <div>
+          <p>HNG Internship 9 Frontend Task</p>
         </div>
-
-        <hr />
-
-        <footer>
-            <div><img src="/img/zuri.svg" alt="Zuri" /></div>
-            <div><p>HNG Internship 9 Frontend Task</p></div>
-            <div><img src="/img/I4G.svg" alt="Ingressive4Good" /></div>
-        </footer>
+        <div>
+          <img src="/img/I4G.svg" alt="Ingressive4Good" />
+        </div>
+      </footer>
     </div>
-  )
+  );
 }
 
 export default Home
